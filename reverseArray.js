2827f6ea45f5de609f6ee0ee52c6cb1681eabdb2
Zubaer16 <48915775+Zubaer16@users.noreply.github.com>
2024-01-08 15:26:10 +0600

    Create reverseArray.js

---
+++ reverseArray.js
@@ -0,0 +1,13 @@
+function reverseArray(arr,start,end){
+    
+    while(start < end){
+    let  temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++
+        end--
+    }
+    return arr
+}
+
+console.log(reverseArray([5,4,3,2,1],0,4))
